1ccacd877c6298d893cc1e648410f4d358bc561d
hakaboom <43911741+hakaboom@users.noreply.github.com>
2021-06-23 23:34:46 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup
 
 setup(
     name='baseImage',
-    version='1.0.4',
+    version='1.0.5',
     author='hakaboom',
     author_email='1534225986@qq.com',
+    license='Apache License 2.0',
     description='This is a secondary package of OpenCV,for manage image data',
     url='https://github.com/hakaboom/base_image',
     packages=['baseImage'],
     install_requires=['colorama>=0.4.4',
                       "loguru>=0.5.3",
+                      "pydantic",
 ],
 )

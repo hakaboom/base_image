c85a3ea5c0f419d0d7db3216bdfab7bc1232dd23
hakaboom <270704881@qq.com>
2021-12-15 23:16:48 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='baseImage',
-    version='1.0.8',
+    version='1.0.9',
     author='hakaboom',
     author_email='1534225986@qq.com',
     license='Apache License 2.0',
     description='This is a secondary package of OpenCV,for manage image data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/hakaboom/base_image',
     packages=['baseImage'],
-    install_requires=['colorama>=0.4.4',
-                      "loguru>=0.5.3",
-                      "pydantic",
-],
+    install_requires=[
+        'numpy>=1.21.4',
+        'opencv-python>=4.5.4.60'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3, <=3.10',
 )

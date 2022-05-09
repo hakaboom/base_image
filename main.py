1839022549f3acbfc0e9f49ce10b22b4bff1e162
huangziqing <270704881@qq.com>
2022-05-09 18:33:06 +0800

    update version

--- main.py
+++ main.py
@@ -2,7 +2,16 @@
 python setup.py sdist
 twine upload dist/*
 """
-import cv2
-from baseImage import Image
-import os
+import time
 
+import cv2
+from baseImage import Setting
+
+cv2.cuda.setBufferPoolUsage(True)
+cv2.cuda.setBufferPoolConfig(cv2.cuda.getDevice(), 1024 * 1024 * (3 + 3), 1)
+
+stream = cv2.cuda.Stream()
+pool = cv2.cuda.BufferPool(stream)
+
+Setting.Default_Stream = stream
+Setting.Default_Pool = pool

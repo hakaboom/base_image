ebc52ea42d6e4f1c5dcb088709bd72411fa2e453
David Chen <30871402+clgwlg@users.noreply.github.com>
2023-06-05 14:57:25 +0800

    Update python version control

--- setup.py
+++ setup.py
@@ -39,5 +39,5 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6, <=3.10',
+    python_requires='>=3.6, <3.11',
 )
